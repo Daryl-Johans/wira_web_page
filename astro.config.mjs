3998bdf1ec8aecdc94dc68b18a77463febc3fe45
Daryl-Johans <darylescrocraxker@gmail.com>
2026-07-06 02:42:37 -0400

    Configurar despliegue automático en GitHub Pages mediante GitHub Actions

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,8 @@ import { defineConfig } from 'astro/config';
 
 // Configuracion del arnes Astro para Wira Web Page
 export default defineConfig({
-  site: 'https://wira.systems',
+  site: 'https://Daryl-Johans.github.io',
+  base: '/wira_web_page',
   compressHTML: true,
   build: {
     inlineStylesheets: 'auto',
